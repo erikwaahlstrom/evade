846085921d6c9d64d9e0d59ec346983d447ba218
Erik Wahlström <erik93_93@hotmail.com>
2020-01-27 09:56:57 +0100

    commit

---
+++ src/components/Character.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1>Character</h1>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
---
+++ src/components/GameCanvas.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <canvas ref="game" class="canvas"></canvas>
+    <character></character>
+  </div>
+</template>
+
+<script>
+import Character from "./Character";
+export default { components: { Character } };
+</script>
+
+<style>
+.canvas {
+  width: 640px;
+  height: 480px;
+  border: 1px solid black;
+}
+</style>
